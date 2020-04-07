f38f5381da5a727777aa955b89af379822dcdab3
kirizaki919 <36561906+kirizaki919@users.noreply.github.com>
2020-04-07 15:43:46 +0800

    Add files via upload
    0.2.1 update lel

--- KVG/Items/Zangetsu.cs
+++ KVG/Items/Zangetsu.cs
@@ -1,3 +1,5 @@
+using Terraria;
+using Microsoft.Xna.Framework;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -11,28 +13,71 @@ namespace KVG.Items
 		public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Zangetsu");
-                glowMask = GlowMaskAPI.Tools.instance.AddGlowMask(mod.GetTexture("Items/Zangetsu_Glow"));
+			Tooltip.SetDefault("Sugei Na");
+            glowMask = GlowMaskAPI.Tools.instance.AddGlowMask(mod.GetTexture("Items/Zangetsu_Glow"));
         }
 
         public override void SetDefaults()
         {
-			item.damage = 200000;
 			item.melee = true;
 			item.width = 20;
 			item.height = 20;
-			item.useTime = 10;
-			item.useAnimation = 10;
 			item.useStyle = 1;
 			item.knockBack = 100;
 			item.value = 10000;
 			item.rare = 10;
-			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
-            item.glowMask=glowMask;
-            item.shoot = mod.ProjectileType("GetsugaTenshou");
-            item.shootSpeed = 8f;
+			item.glowMask = glowMask;
         }
 
+		public override bool AltFunctionUse(Player player)
+		{
+		return true;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+
+		if (player.altFunctionUse == 2)
+		{
+			item.damage = 200000;
+			item.melee = true;
+			item.width = 20;
+			item.height = 20;
+			item.mana = 5;
+			item.useTime = 80;
+			item.useAnimation = 80;
+			item.useStyle = 1;
+			item.knockBack = 100;
+			item.autoReuse = true;
+            item.shoot = mod.ProjectileType("GetsugaTenshou");
+            item.shootSpeed = 3f;
+			item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/GetsugaSound");
+			item.healMana = 10;
+			item.healLife = 10;
+		}
+
+		else
+		{
+			item.damage = 200;
+			item.melee = true;
+			item.width = 20;
+			item.height = 20;
+			item.mana = 0;
+			item.useTime = 10;
+			item.useAnimation = 10;
+			item.useStyle = 1;
+			item.knockBack = 100;
+			item.autoReuse = true;
+			item.shoot = 0;
+			item.UseSound = SoundID.Item1;
+		}
+		return true;
+		}
+
+        public override Vector2? HoldoutOffset() {
+        return Vector2.Zero;
+        }
 
 		public override void AddRecipes() 
 		{
--- KVG/Projectiles/GetsugaTenshou.cs
+++ KVG/Projectiles/GetsugaTenshou.cs
@@ -4,29 +4,47 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
 
 namespace KVG.Projectiles
 {
 
-public class GetsugaTenshou : ModProjectile
-{
-public override void SetDefaults()
-{
+      public class GetsugaTenshou : ModProjectile
+  {
+      public override void SetDefaults()
+    {
 
-projectile.width = 100; //projectile width
-projectile.height = 100; //projectile height
-projectile.friendly = true; //make that the projectile will not damage you
-projectile.melee = true; //
-projectile.tileCollide = false; //make that the projectile will be destroed if it hits the terrain
-projectile.penetrate = 30; //how many npc will penetrate
-projectile.timeLeft = 9999; //how many time this projectile has before disepire
-projectile.light = 1.00f; // projectile light
-projectile.extraUpdates = 1;
-projectile.ignoreWater = true;
-}
-public override void AI() //this make that the projectile will face the corect way
-{ // |
-projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 0.3f;
-}
-}
+     projectile.width = 500; //projectile width
+     projectile.height = 500; //projectile height
+     projectile.friendly = true; //make that the projectile will not damage you
+     projectile.melee = true; //
+     projectile.tileCollide = false; //make that the projectile will be destroed if it hits the terrain
+     projectile.penetrate = 9999; //how many npc will penetrate
+     projectile.timeLeft = 300; //how many time this projectile has before disepire
+     projectile.light = 10f; // projectile light
+     projectile.extraUpdates = 1;
+     projectile.ignoreWater = true;
+     projectile.soundDelay = 1;
+     projectile.alpha = 255;
+ 
+     }
+
+
+
+     public override void AI() //this make that the projectile will face the corect way
+     { projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 0.3f;
+			// Speed up
+			projectile.ai[0] += 0.5f;
+            if (projectile.ai[0] > 1f){
+               projectile.alpha -= 3;     
+			}
+
+
+			projectile.velocity *= 0.95f;
+			// Speed up after 60 frame = 1 seconds
+			if (++projectile.frameCounter >= 120) {
+				projectile.velocity *=1.5f;
+			}
+     }
+   }
 }
